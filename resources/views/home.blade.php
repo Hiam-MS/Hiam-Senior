cf98afe85e376d64bdfaea559ca214150cd287e7
Hiam-MS <hiam.ms98@gmail.com>
2021-06-04 21:02:33 +0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+    <div class="col-md-3">
+        <div class="users">
+        <h5>Users</h5>
+        <ul class="list-group list-chat-item">
+            <li class="chat-user-list">
+                <a href="">
+                    Hiam
+                </a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    <div class="col-md-9">
+
+    </div>
+</div>
+@endsection
