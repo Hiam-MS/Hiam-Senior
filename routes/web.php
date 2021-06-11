d27ab333cee1c20c25964f2aeefe12fcdce57d42
Hiam-MS <hiam.ms98@gmail.com>
2021-06-11 20:51:07 +0300

    F

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,33 @@ Route::get('/consultant/profile', function () {
 Route::get('/submittal/ini', function () {
     return view('/Submittal/Initiator');
 });
+
+Route::get('/submittal/cont_sent', function () {
+    return view('/Submittal/Contractor_Sent');
+});
+
+Route::get('/submittal/cons_recv', function () {
+    return view('/Submittal/Consultant_Recieve');
+});
+
+Route::get('/submittal/cons_team_recv', function () {
+    return view('/Submittal/Consultant_Team_Reply');
+});
+
+Route::get('/submittal/cons_rep', function () {
+    return view('/Submittal/Consultant_Reply');
+});
+
+Route::get('/submittal/cont_recv', function () {
+    return view('/Submittal/Contractor_Recv');
+});
+Route::get('/submittal/main', function () {
+    return view('/Submittal/Main_Submittals');
+});
+Route::get('/submittal/log', function () {
+    return view('/Submittal/Log_Submittals');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
